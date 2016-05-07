171ccff0e60bd294482b32e6cfba83bd90b03f1f
Julio Andrade <julio@MBP.local>
2016-05-07 14:14:52 -0500

    Moved displayBits into its own header [lazy programming]

--- Bitwise AND inclusive OR/bitwiseExercise.cpp
+++ Bitwise AND inclusive OR/bitwiseExercise.cpp
@@ -1,6 +1,7 @@
 // Copyright Julio Andrade
 // This is just a program I made as an exercise, practicing bitwise operations.
 
+#include "displayBits.h" // displayBits function
 #include <iostream>
 #include <iomanip>
 using namespace std;
@@ -49,24 +50,3 @@ int main()
 	cout << "is" << endl;
 	displayBits(~number1);
 } // end main
-
-// display bits o an unsigned integer value
-void displayBits(unsigned value)
-{
-	const int SHIFT = 8 * sizeof(unsigned) - 1;
-	const unsigned MASK = 1 << SHIFT; // mask = 1000 000 000 000 rn
-	
-	cout << setw(10) << value << " = ";
-
-	// display bits
-	for (unsigned i = 1; i <= SHIFT + 1; ++ i)
-	{
-	  cout << (value & MASK ? '1' : '0');
-	value <<= 1; // shift value left by 1
-
-	if (i % 8 == 0) // every 8 bits
-	  cout << ' '; // output space
-	}
-  cout << endl;
-}
-
