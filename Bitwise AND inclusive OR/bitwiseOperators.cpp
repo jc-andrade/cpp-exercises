49e5624d3153c2bc4eb239dc5ffde444b9f19a13
Julio Andrade <julio@MBP.local>
2016-05-07 14:19:02 -0500

    Working bitwiseOperators

--- Bitwise AND inclusive OR/bitwiseOperators.cpp
+++ Bitwise AND inclusive OR/bitwiseOperators.cpp
@@ -8,4 +8,12 @@ int main()
 	// demonstrate bitwise left shift
 	cout << "The result of left shifting\n";
 	displayBits(number1);
+	cout << "8 bit positions using the left-shift operator is\n";
+	displayBits(number1 << 8);
+
+	/// demonstrate bitwise right shift
+	cout << "\nThe result of right shifting\n";
+	displayBits(number1);
+	cout << "8 bit positions using the right-shift operator is\n";
+	displayBits(number1 >> 8);
 }
